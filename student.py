c79bd4e5ae55e565cd0e15fcf2dd56508b0d71d2
flomonster <florian.amsallem@hotmail.fr>
2017-02-14 11:39:23 +0530

    ADD student class

---
+++ student.py
@@ -0,0 +1,10 @@
+class Student:
+
+    def __init__(self, firstname = '', lastname = '', roll_no = '', subjects = []):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.roll_no = roll_no
+        self.subjects = subjects
+
+    def load(path):
+        pass
