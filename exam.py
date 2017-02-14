598ba2ed00f231cb23c739bc7feb8f020d291f7e
swapme3 <athawale.1@iitj.ac.in>
2017-02-14 11:57:39 +0530

    add slot and exam class

---
+++ exam.py
@@ -0,0 +1,16 @@
+class Exam:
+	def _init_(self, subjects=[], time_slot=[]):
+
+		self.student = []
+		self.classroom = []
+		self.teacher = []
+		self.tas = []
+		self.subjects = subjects
+		self.time_slot = time_slot
+
+	def schedule():
+		pass
+
+	def print():
+		pass
+
---
+++ slot.py
@@ -0,0 +1,17 @@
+class Slot:
+	def _init_(self, student=[], classroom=[], teacher=[], tas=[], subjects=[], time_slot=[]):
+
+		self.student = student
+		self.classroom = classroom
+		self.teacher = teacher
+		self.tas = tas
+		self.subjects = subjects
+		self.time_slot = time_slot
+
+	def manage():
+		pass
+
+	def print():
+		pass
+	
+		
