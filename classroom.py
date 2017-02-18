65e14a1edae03f47de2961439233c81d4fd406e0
swapme <athawale.1@iitj.ac.in>
2017-02-18 17:51:57 +0530

    added classroom load functon and classroom dta file

--- classroom.py
+++ classroom.py
@@ -1,3 +1,4 @@
+import xlrd
 class Classroom:
 
     def __init__(self, name = '', n_row = 0, n_column = 0):
@@ -9,7 +10,18 @@ class Classroom:
         self.n_column = n_column
 
     def load(path):
-        pass
-    
+        book = xlrd.open_workbook(path)
+        sheet = book.sheet_by_index(0)
+        cr_list = []
+
+        for i in range(1, sheet.nrows):
+            row = sheet.row_values(i)
+            cr_list.append(Classroom(row[0], int(row[1]), int(row[2])))
+           
+        return cr_list
+   
     def print(self, exam_name, time_slot):
         pass
+        
+if __name__=='__main__':
+    print(Classroom.load("classroom.xlsx"))
