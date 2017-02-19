0a2f99315eaff5856607c41c0d8f7626be0fe272
flomonster <florian.amsallem@hotmail.fr>
2017-02-19 17:00:44 +0530

    ADD print of classroom map

--- classroom.py
+++ classroom.py
@@ -1,4 +1,10 @@
 import xlrd
+from reportlab.lib import colors
+from reportlab.platypus import PageBreak
+from reportlab.lib.pagesizes import A4, inch, landscape
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
+
 class Classroom:
 
     def __init__(self, name = '', n_row = 0, n_column = 0):
@@ -20,7 +26,44 @@ class Classroom:
            
         return cr_list
    
-    def print(self, exam_name, time_slot):
-        pass
-        
+    def printPdf(self, exam_name, time_slot):
+        filename = exam_name + '_' + time_slot[0] + '-' + str(time_slot[1]) + '_' + self.name + '.pdf'
+        doc = SimpleDocTemplate(filename, pagesize=A4, rightMargin=30,leftMargin=30, topMargin=30,bottomMargin=18)
+        doc.pagesize = landscape(A4)
 
+        # Classroom table
+        data = [[''] * self.n_column for i in range(self.n_row + 3)]
+        data[0][0] = exam_name
+        data[1][0] = 'Classroom: ' + self.name + ' ' + time_slot[0] + ' ' + \
+                str(time_slot[1] // 100) + ':' + str(time_slot[1] % 100) + ' to ' +\
+                str(time_slot[2] // 100) + ':' + str(time_slot[2] % 100)
+        for i in range (self.n_column):
+            data[2][i] = 'C' + str(i + 1)
+
+        for i in range(self.n_row):
+            for j in range(self.n_column):
+                data[i+3][j] = self.map_student[i][j].roll_no
+
+        style = TableStyle([('SPAN', (0, 0), (-1, 0)),
+                            ('SPAN', (0, 1), (-1, 1)),
+                            ('ALIGN', (0,0), (-1,-1), 'CENTER'),
+                            ('VALIGN', (0,0), (-1,-1), 'MIDDLE'),
+                            ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+                            ('BOX', (0,0), (-1,-1), 0.25, colors.black),
+                            ])
+        table = Table(data)
+        table.setStyle(style)
+
+        # Teacher table
+        data2 = [[''] for i in range(3 + len(self.tas))]
+        data2[0][0] = exam_name
+        data2[1][0] = 'Classroom: ' + self.name + ' ' + time_slot[0] + ' ' + \
+                str(time_slot[1] // 100) + ':' + str(time_slot[1] % 100) + ' to ' +\
+                str(time_slot[2] // 100) + ':' + str(time_slot[2] % 100)
+        data2[2][0] = self.teacher.lastname + ' ' + self.teacher.firstname
+        for i, t in enumerate(self.tas):
+            data2[3 + i][0] = t.lastname + ' ' + t.firstname + '\n' + t.roll_no
+        table2 = Table(data2)
+        table2.setStyle(style)
+
+        doc.multiBuild([table, PageBreak(), table2])
